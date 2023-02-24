72a9b1d02fee2db739f2e10eecf980e32b5d9ffd
gilik154 <okk080200@gmail.com>
2023-02-24 15:19:59 +0900

    2023-02-24 v0.01

---
+++ src/test/java/com/example/foodcloud/FoodCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foodcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
