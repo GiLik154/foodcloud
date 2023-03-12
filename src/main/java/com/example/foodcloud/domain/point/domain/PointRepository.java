0dfe19bdfcc5fe071d47deac8b3d82da80c79845
gilik154 <okk080200@gmail.com>
2023-03-13 00:24:10 +0900

    Feat 2023.03.13 todo에 오류 적어둠. 수정해야함.

--- src/main/java/com/example/foodcloud/domain/point/domain/PointRepository.java
+++ src/main/java/com/example/foodcloud/domain/point/domain/PointRepository.java
@@ -2,8 +2,14 @@ package com.example.foodcloud.domain.point.domain;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+
+import javax.persistence.LockModeType;
 
 public interface PointRepository extends JpaRepository<Point, Long> {
     boolean existsByUserId(Long userId);
+
     Point findByUserIdOrderByIdDesc(Long userId);
+
+    //todo 비관적 락 추가해야함 (@Lock(LockModeType.OPTIMISTIC))
 }
